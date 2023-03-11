7e928d8a65f9b13ae7d2ab08fe0c175380fd32f0
bekzod818 <bekzod030900@gmail.com>
2023-03-12 00:14:06 +0500

    fix: nginx conf

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
